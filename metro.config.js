1f0c910b4897a210bd7e5673112c8a93cb01eeaf
hyochan <dooboolab@gmail.com>
2023-07-09 22:37:03 +0900

    build: migrate to expo sdk 49
    https://blog.expo.dev/expo-sdk-49-c6d398cdf740

--- metro.config.js
+++ metro.config.js
@@ -1,4 +1,6 @@
-// Learn more https://docs.expo.io/guides/customizing-metro
 const {getDefaultConfig} = require('expo/metro-config');
 
-module.exports = getDefaultConfig(__dirname);
+const config = getDefaultConfig(__dirname);
+config.resolver.assetExts.push('mjs');
+
+module.exports = config;
