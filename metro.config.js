6c37b0ca708d2849a6b9a55e71e1c5ca4ed9e09a
hyochan <dooboolab@gmail.com>
2023-08-05 22:05:40 +0900

    chore: isCSSEnabled option in metro.config.js
    Tried `isCSSEnabled` option but still not working on web. https://github.com/emotion-js/emotion/issues/3073\#issuecomment-1666384565

--- metro.config.js
+++ metro.config.js
@@ -2,7 +2,9 @@
 const {getDefaultConfig} = require('expo/metro-config');
 
 /** @type {import('expo/metro-config').MetroConfig} */
-const config = getDefaultConfig(__dirname);
+const config = getDefaultConfig(__dirname, {
+  isCSSEnabled: true,
+});
 config.resolver.assetExts.push('mjs');
 
 module.exports = config;
