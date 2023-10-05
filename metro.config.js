a034d1dc48bd8a6f99522e94354bf633b87fca5d
hyochan <dooboolab@gmail.com>
2023-10-06 02:39:16 +0900

    chore: update packages

--- metro.config.js
+++ metro.config.js
@@ -1,9 +1,7 @@
-// Learn more https://docs.expo.io/guides/customizing-metro
 const {getDefaultConfig} = require('expo/metro-config');
 
-/** @type {import('expo/metro-config').MetroConfig} */
-const config = getDefaultConfig(__dirname, {
-  isCSSEnabled: true,
-});
+const config = getDefaultConfig(__dirname);
+
+config.resolver.sourceExts.push('cjs', 'mjs');
 
 module.exports = config;
