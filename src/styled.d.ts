668d11e54f81828ef6c9324a0e5714f737324143
hyochan <dooboolab@gmail.com>
2023-11-23 19:10:52 +0900

    chore(types): override emotion styles

--- src/styled.d.ts
+++ src/styled.d.ts
@@ -1,6 +1,8 @@
 import '@emotion/react';
 import type {DoobooTheme as DoobooUiTheme} from '@dooboo-ui/theme';
 import type {CustomAppTheme} from './theme';
+import {CSSObject} from '@emotion/react';
+import {StyleProp, ViewStyle} from 'react-native';
 
 type AllTheme = CustomAppTheme & DoobooUiTheme;
 
@@ -10,4 +12,21 @@ declare module '@emotion/react' {
 
 declare module 'dooboo-ui' {
   export interface DoobooUiTheme extends AllTheme {}
+}
+
+declare module '@emotion/native' {
+  // Overload for ViewStyle
+  export function css(
+    ...args: Array<CSSObject | StyleProp<ViewStyle>>
+  ): ReturnType<typeof css>;
+
+  // Overload for TextStyle
+  export function css(
+    ...args: Array<CSSObject | StyleProp<TextStyle>>
+  ): ReturnType<typeof css>;
+
+  // Overload for ImageStyle
+  export function css(
+    ...args: Array<CSSObject | StyleProp<ImageStyle>>
+  ): ReturnType<typeof css>;
 }
